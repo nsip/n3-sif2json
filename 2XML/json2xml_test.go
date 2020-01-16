fb0ac67f5257d612478fb25b7298a495d7d79515
Qing Miao <cdutwhu@outlook.com>
2020-01-16 11:10:13 +1100

    include <SpecUtil> sub project

--- 2XML/json2xml_test.go
+++ 2XML/json2xml_test.go
@@ -3,5 +3,5 @@ package cvt2xml
 import "testing"
 
 func TestJSON2XML(t *testing.T) {
-	json2xml("../data/test.json", "../data/test1.xml")
+	json2xml("../data/Activity.json", "../data/Activity1.xml")
 }
