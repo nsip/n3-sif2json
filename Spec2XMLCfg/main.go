6081158ee04bbe5b813daca712c9b978df6b67f2
Qing Miao <cdutwhu@outlook.com>
2020-01-31 16:36:44 +1100

    + ScanOA

--- Spec2XMLCfg/main.go
+++ Spec2XMLCfg/main.go
@@ -6,7 +6,8 @@ import (
 	cmn "github.com/cdutwhu/json-util/common"
 )
 
-func initMapOfObjAttrs(xpathGrp []string, sep string) {
+// InitMapOfObjAttrs :
+func InitMapOfObjAttrs(xpathGrp []string, sep string) {
 	for _, xpath := range xpathGrp {
 		ss := sSplit(xpath, sep)
 		attr, attrType, objType := ss[0], ss[1], ss[4]
@@ -14,11 +15,6 @@ func initMapOfObjAttrs(xpathGrp []string, sep string) {
 		mObjIdxOfAttr[objType] = 0
 		mOAType[attr] = attrType
 	}
-}
-
-// RewindAttrIter :
-func RewindAttrIter(objType string) {
-	mObjIdxOfAttr[objType] = 0
 }
 
 // NextAttr :
@@ -59,6 +55,7 @@ func PrintXML(paper, line, contentHolder string, iLine int, tag string) (string,
 func SortSimpleObject(xml, obj string, level int) (paper string) {
 	defer func() {
 		fPln(paper)
+		resetPrt()
 	}()
 
 	const INDENT = "    " // 4 space
@@ -83,7 +80,7 @@ func SortSimpleObject(xml, obj string, level int) (paper string) {
 	//	NEXTOBJ:
 	for t := 0; t < nObj; t++ {
 
-		RewindAttrIter(mOAType[obj])
+		rewindAttrIter(mOAType[obj])
 		PS, PE := 0, 0
 
 		for i, l := range lines {
@@ -202,7 +199,7 @@ func main() {
 	// 	fPln(obj, mObjAttrs[obj])
 	// }
 
-	initMapOfObjAttrs(xpathGrp, SEP)
+	InitMapOfObjAttrs(xpathGrp, SEP)
 
 	// value, end := NextAttr("SoftwareRequirement")
 	// for ; !end; value, end = NextAttr("SoftwareRequirement") {
@@ -211,6 +208,60 @@ func main() {
 
 	bytes, err = ioutil.ReadFile("../data/Activity1.xml")
 	cmn.FailOnErr("%v", err)
-	// SortSimpleObject(string(bytes), "ActivityTime", 1)
-	SortSimpleObject(string(bytes), "Activity", 0)
+	// SortSimpleObject(string(bytes), "SoftwareRequirement", 2)
+
+	ScanOA(string(bytes))
+}
+
+// --------------------------------------- //
+
+// ScanOA :
+func ScanOA(xml string) {
+
+	var (
+		mLvlOAs = make(map[int][]string)
+	)
+
+	ss := sSplit(xml, "\n")
+	for _, l := range ss {
+		lvl, sl := 0, 0
+		switch {
+		case sHasPrefix(l, S0) && !sHasPrefix(l, E0):
+			lvl, sl = 0, len(S0)
+		case sHasPrefix(l, S1) && !sHasPrefix(l, E1):
+			lvl, sl = 1, len(S1)
+		case sHasPrefix(l, S2) && !sHasPrefix(l, E2):
+			lvl, sl = 2, len(S2)
+		case sHasPrefix(l, S3) && !sHasPrefix(l, E3):
+			lvl, sl = 3, len(S3)
+		case sHasPrefix(l, S4) && !sHasPrefix(l, E4):
+			lvl, sl = 4, len(S4)
+		case sHasPrefix(l, S5) && !sHasPrefix(l, E5):
+			lvl, sl = 5, len(S5)
+		case sHasPrefix(l, S6) && !sHasPrefix(l, E6):
+			lvl, sl = 6, len(S6)
+		case sHasPrefix(l, S7) && !sHasPrefix(l, E7):
+			lvl, sl = 7, len(S7)
+		case sHasPrefix(l, S8) && !sHasPrefix(l, E8):
+			lvl, sl = 8, len(S8)
+		case sHasPrefix(l, S9) && !sHasPrefix(l, E9):
+			lvl, sl = 9, len(S9)
+		case sHasPrefix(l, S10) && !sHasPrefix(l, E10):
+			lvl, sl = 10, len(S10)
+		case sHasPrefix(l, S11) && !sHasPrefix(l, E11):
+			lvl, sl = 11, len(S11)
+		default:
+			continue
+		}
+
+		oa := cmn.RmTailFromFirstAny(l[sl:], " ", ">")
+		mLvlOAs[lvl] = append(mLvlOAs[lvl], oa)
+	}
+
+	for i := 0; i < 11; i++ {
+		fPln(i, mLvlOAs[i])
+		for _, oa := range mLvlOAs[i] {
+			SortSimpleObject(xml, oa, i)
+		}
+	}
 }
--- Spec2XMLCfg/var.go
+++ Spec2XMLCfg/var.go
@@ -26,4 +26,46 @@ var (
 	mOAType       = make(map[string]string)   // key: obj
 	mOAPrtLn      = make(map[string]int)      // key: obj
 	objGrp        []string
+
+	rewindAttrIter = func(objType string) {
+		mObjIdxOfAttr[objType] = 0
+	}
+
+	resetPrt = func() {
+		mOAPrtLn = make(map[string]int)
+	}
+
+	mkIndent = func(n int) (indent string) {
+		const INDENT = "    " // 4 space
+		for i := 0; i < n; i++ {
+			indent += INDENT
+		}
+		return
+	}
+
+	S0  = mkIndent(0) + "<"
+	S1  = mkIndent(1) + "<"
+	S2  = mkIndent(2) + "<"
+	S3  = mkIndent(3) + "<"
+	S4  = mkIndent(4) + "<"
+	S5  = mkIndent(5) + "<"
+	S6  = mkIndent(6) + "<"
+	S7  = mkIndent(7) + "<"
+	S8  = mkIndent(8) + "<"
+	S9  = mkIndent(9) + "<"
+	S10 = mkIndent(10) + "<"
+	S11 = mkIndent(11) + "<"
+
+	E0  = S0 + "/"
+	E1  = S1 + "/"
+	E2  = S2 + "/"
+	E3  = S3 + "/"
+	E4  = S4 + "/"
+	E5  = S5 + "/"
+	E6  = S6 + "/"
+	E7  = S7 + "/"
+	E8  = S8 + "/"
+	E9  = S9 + "/"
+	E10 = S10 + "/"
+	E11 = S11 + "/"
 )
