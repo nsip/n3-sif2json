17b4c33ac743901242261a65919625e99858edeb
Qing Miao <cdutwhu@outlook.com>
2020-05-25 17:01:41 +1000

    * set JAEGER_SERVICE_NAME

--- Server/main.go
+++ Server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	eg "github.com/cdutwhu/n3-util/n3errs"
 	g "github.com/nsip/n3-sif2json/Server/global"
 	api "github.com/nsip/n3-sif2json/Server/webapi"
@@ -14,6 +16,8 @@ func main() {
 
 	setLog(logfile)
 	fPln(logWhen(true, "[%s] Hosting on: [%v:%d], version [%v]", servicename, localIP(), ws.Port, ws.Version))
+
+	os.Setenv("JAEGER_SERVICE_NAME", servicename)
 
 	done := make(chan string)
 	go api.HostHTTPAsync()
